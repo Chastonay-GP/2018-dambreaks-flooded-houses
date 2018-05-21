604d97b544f55e3c66355dbed540c78f3b97b59a
DominikSVogt <37217400+DominikSVogt@users.noreply.github.com>
2018-05-21 22:13:11 +0200

    first complet network approach

--- scratch.py
+++ scratch.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import itertools
 import shapefile
+import numpy as np
 
 #Create empty graph
 G=nx.Graph()
@@ -42,7 +43,7 @@ Elem_R=map(int,Elem_R)
 #To create Edges we combine the two Node rows in a single tuple.
 #E.g. Node1=(1,2,3) and Node2=(a,b,c) => Edges=(1,a),(2,b),(3,b)
 Tuple_Nodes = zip(Node1, Node2)
-print Tuple_Nodes
+#print Tuple_Nodes
 
 #Extend the list from Node2 two the copied list Node1Copy
 Node1Copy.extend(Node2)
@@ -50,6 +51,7 @@ Node1Copy.extend(Node2)
 #Setting Node1 back to the original list. Why ever I need to do this... Do not understand why Python changes the variable Node1 with the code from the previous line
 Node1 = [int(row[1]) for row in rows]
 print nx.info(G)
+
 
 #Adding nodes from Node1Copy --> Error: Too many values??
 #G.add_nodes_from(Node1Copy)
@@ -60,32 +62,47 @@ print nx.info(G)
 #Creating Edges and check the graph info
 G.add_edges_from(Tuple_Nodes)
 print nx.info(G)
-len(Node1Copy)
 
 #Access shapefile for nodes and coordinates
 fields =shape.fields[1:]
 fields_name = [field[0] for field in fields] #-> the fields names in a list
-print fields_name
+#print fields_name
 attributes = shape.records()
-print attributes
+#print attributes
+#Idee: Von shp die nodes nehmen und dabei die Georefernzierung der Punkte verwenden, da ansonsten networkX überlastet ist mit 20'000 punkten
 
-#--------------------------------------------------------------------------------
+#Extract the nested list into a simplier list
+Attributes_List_Simple=[]
+for inner_l in attributes:
+    for item in inner_l:
+        Attributes_List_Simple.append(item)
+
+#extract nodes, x- and y-position from attribute table from shapefile
+Attribute_nodes = Attributes_List_Simple[0::4]
+Attribute_xPos = Attributes_List_Simple [1::4]
+Attribute_yPos = Attributes_List_Simple[2::4]
+Attribute_pos = zip(Attribute_xPos,Attribute_yPos)
+
+#Adding nodes to Graph
+G.add_nodes_from(Attribute_nodes)
 print nx.info(G)
 
-pe = nx.Graph()
-pe.add_node('p1', posxy=(ap[0][0], ap[0][1]))
-pe.add_node('p2', posxy=(ap[1][0], ap[1][2]))
-pe.add_node('p3', posxy=(ap[2][0], ap[2][3]))
-pe.add_node('p4', posxy=(ap[3][0], ap[3][4]))
-pe.add_node('p5', posxy=(ap[4][0], ap[4][5]))
-pe.add_cycle(['p1','p2','p3','p4','p5'])
-positions = nx.get_node_attributes(pe,'posxy')
-nx.draw(pe, positions, node_size=500)
-#nx.draw_networkx(G)
-#nx.draw_networkx_edges(G)
-#plt.show(G)
-#nx.draw(G)
-#nx.draw(G,pos=nx.spring_layout(G))
+#creating empty dictionary
+d={}
+#fill dictionary with nodes as keys and xy-position as values
+d=dict(zip(Attribute_nodes,Attribute_pos))
+nx.draw_networkx(G,pos=d)
 
-
-
+#--------------------------------------------------------------------------------
+Test=nx.read_shp('C:/Users/Dominik/OneDrive/Dokumente/UniBe/Master/FS18/Geodatenanalyse/Projekt/VogtEugster/VersionGrobaufloesung/aareV10rechtsGrob_Quality_nodes.shp')
+Test = shapefile.Reader('C:/Users/Dominik/OneDrive/Dokumente/UniBe/Master/FS18/Geodatenanalyse/Projekt/VogtEugster/VersionGrobaufloesung/aareV10rechtsGrob_Quality_nodes.shp')
+pos = {k: v for k,v in enumerate(Test.nodes())}
+X=nx.Graph() #Empty graph
+X.add_nodes_from(pos.keys()) #Add nodes preserving coordinates
+l=[set(x) for x in Test.edges()] #To speed things up in case of large objects
+edg=Tuple_Nodes
+X.add_edges_from(edg)
+#edg=[tuple(k for k,v in pos.items() if v in sl) for sl in l] #Map the G.edges start and endpoints onto pos
+nx.draw_networkx_nodes(X,pos,node_size=100,node_color='r')
+X.add_edges_from(edg)
+nx.draw_networkx_edges(X,pos)
