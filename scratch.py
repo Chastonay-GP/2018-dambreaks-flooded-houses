67897cc2a260c8b36d303df655ccf38295d83e84
DominikSVogt <37217400+DominikSVogt@users.noreply.github.com>
2018-05-26 19:22:22 +0200

    Added attributes to networkX
    corrected items in dictionary as float

--- scratch.py
+++ scratch.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import itertools
 import shapefile
 import numpy as np
+import re
+import os
 
+#Plotting the network on basis of the edges from the .edg file (output of basement-simulation) and the nodes from a shapefile
+#---------------------------------------
 #Create empty graph
 G=nx.Graph()
 #M=nx.DiGraph()
@@ -52,13 +56,6 @@ Node1Copy.extend(Node2)
 Node1 = [int(row[1]) for row in rows]
 print nx.info(G)
 
-
-#Adding nodes from Node1Copy --> Error: Too many values??
-#G.add_nodes_from(Node1Copy)
-#type(Node1Copy)
-#len(Node1Copy)
-#G.nodes()
-
 #Creating Edges and check the graph info
 G.add_edges_from(Tuple_Nodes)
 print nx.info(G)
@@ -91,18 +88,61 @@ print nx.info(G)
 d={}
 #fill dictionary with nodes as keys and xy-position as values
 d=dict(zip(Attribute_nodes,Attribute_pos))
-nx.draw_networkx(G,pos=d)
+#warnings: internet forum suggest to ignore the warnings: https://github.com/networkx/networkx/issues/2407
+nx.draw(G,pos=d, node_size=10) # ist um einiges schneller wie nx.draw_networkx
+nx.draw_networkx(G,pos=d, node_size=10, with_labels=False)
 
+#Adding attributes to the nodes (water depth, velocity and water surface elevation)
+#---------------------------------------------------------------------------
+#create outer dictionary with TS1 - TS25 as keys
+Steps=range(1,26)
+Timesteps=[]
+for i in Steps:
+    print "TS"+str(i)
+    Timesteps.append("TS"+str(i))
+    i=i+1
+Timesteps_tuple=tuple(Timesteps)
+
+Depth_TS_out={}
+Depth_TS_out=dict.fromkeys(Timesteps)
+
+#next step: integrating above code into for loope with splitting .sol-file into timesteps and add them to the subdictionarys from 'Depth_TS_out'
+splitLen = 21134
+outputBase = 'Depth_TS'
+input = open('C:/Users/Dominik/OneDrive/Dokumente/UniBe/Master/FS18/Geodatenanalyse/Projekt/VogtEugster/VersionGrobaufloesung/BASEMENT/aare_nds_depth.sol','r').read().strip('\n').split()
+
+datavalues=[]
+i=3
+k=21136
+for line in input:
+    if line.startswith("TS"):
+        outputData = (input[i:k])
+        datavalues.append(outputData)
+        i=i+1
+        k=k+1
+    else:
+        i=i+1
+        k=k+1
+
+#converting string in datavalues to float
+datavalues_float=[]
+for parts in datavalues:
+    datavalues_float.append([float(q) for q in parts])
+
+datavalues_tuple=tuple(datavalues_float)
+
+#NetworkX needs a dictionary for the attribute.
+#Creating dictionary with timesteps as keys and datavalues (depth) as values.
+Depth_TS={}
+Depth_TS=dict(zip(Timesteps_tuple,datavalues_tuple))
+
+#adding Depth_TS as attributes to the graph
+for timesteps in Depth_TS:
+    nx.set_node_attributes(G, 'Water_Depth', Depth_TS[timesteps])
+
+nx.draw_networkx(G,pos=d, node_size=10, with_labels=False)
+
+#Above lines ok
 #--------------------------------------------------------------------------------
-Test=nx.read_shp('C:/Users/Dominik/OneDrive/Dokumente/UniBe/Master/FS18/Geodatenanalyse/Projekt/VogtEugster/VersionGrobaufloesung/aareV10rechtsGrob_Quality_nodes.shp')
-Test = shapefile.Reader('C:/Users/Dominik/OneDrive/Dokumente/UniBe/Master/FS18/Geodatenanalyse/Projekt/VogtEugster/VersionGrobaufloesung/aareV10rechtsGrob_Quality_nodes.shp')
-pos = {k: v for k,v in enumerate(Test.nodes())}
-X=nx.Graph() #Empty graph
-X.add_nodes_from(pos.keys()) #Add nodes preserving coordinates
-l=[set(x) for x in Test.edges()] #To speed things up in case of large objects
-edg=Tuple_Nodes
-X.add_edges_from(edg)
-#edg=[tuple(k for k,v in pos.items() if v in sl) for sl in l] #Map the G.edges start and endpoints onto pos
-nx.draw_networkx_nodes(X,pos,node_size=100,node_color='r')
-X.add_edges_from(edg)
-nx.draw_networkx_edges(X,pos)
+#lines bellow in testing
+#---------------------------------------
